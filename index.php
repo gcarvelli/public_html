5599ca0599fbd9d375865f3915b23a6ab3be2c53
Giovanni Carvelli <giovannircarvelli@gmail.com>
2015-09-29 01:38:37 -0400

    Added "see on Githug" footer

--- index.php
+++ index.php
@@ -2,42 +2,51 @@
 <?php require_once 'head.php'; ?>
 <body>
 
-<div class="container">
-  <div class="jumbotron">
-    <h1>Giovanni Carvelli</h1>
-    <p>Hello! My name is Gio and I'm a 4<sup>th</sup> year Software Engineering major at RIT.</p> 
-    <button type="button" class="btn btn-primary about-button">About me</button>
-  </div>
+<div id="wrap">
+  <div class="container">
+    <div class="jumbotron">
+      <h1>Giovanni Carvelli</h1>
+      <p>Hello! My name is Gio and I'm a 4<sup>th</sup> year Software Engineering major at RIT.</p> 
+      <button type="button" class="btn btn-primary about-button">About me</button>
+    </div>
 
-  <div class="row about-me">
-    <div class="col-xs-10 col-xs-offset-1">
-      <p>This is the about me text!</p>
+    <div class="row about-me">
+      <div class="col-xs-10 col-xs-offset-1">
+        <p>This is the about me text!</p>
+      </div>
     </div>
-  </div>
-  <div class="row">
-    <div class="col-xs-4">
-      <a href="https://www.linkedin.com/in/giovannircarvelli">
-        <div class="image-container">
-          <img src="img/linkedin.png" width="100%" class="page-link" />
-        </div>
-      </a>
-    </div>
-    <div class="col-xs-4">
-      <a href="./GCarvelliResume.pdf">
-        <div class="image-container">
-          <img src="img/resume.png" width="100%" class="page-link" />
-        </div>
+    <div class="row">
+      <div class="col-xs-4">
+        <a href="https://www.linkedin.com/in/giovannircarvelli">
+          <div class="image-container">
+            <img src="img/linkedin.png" width="100%" class="page-link" />
+          </div>
         </a>
-    </div>
-    <div class="col-xs-4">
-      <a href="http://github.com/gcarvelli">
-        <div class="image-container">
-          <img src="img/Octocat.png" width="100%" class="page-link" />
-        </div>
-      </a>
+      </div>
+      <div class="col-xs-4">
+        <a href="./GCarvelliResume.pdf">
+          <div class="image-container">
+            <img src="img/resume.png" width="100%" class="page-link" />
+          </div>
+          </a>
+      </div>
+      <div class="col-xs-4">
+        <a href="http://github.com/gcarvelli">
+          <div class="image-container">
+            <img src="img/Octocat.png" width="100%" class="page-link" />
+          </div>
+        </a>
+      </div>
     </div>
   </div>
 </div>
 
-
-</body></html>
+</body>
+<div id="footer">
+  <div class="container">
+    <p class="text-muted">Source for this website is available on
+      <a href="http://github.com/gcarvelli/public_html">Github</a>.
+    </p>
+  </div>
+</div>
+</html>
